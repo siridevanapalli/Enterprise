c60b38390f674fc4897750661d660e98ed923802
siridevanapalli <98391637+siridevanapalli@users.noreply.github.com>
2022-04-29 19:05:23 +0530

    Create Auth.groovy

---
+++ Auth.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+// Install SFPowerkit
+pipeline {
+  agent any
+  stages {
+    stage('Authorize an Org') {
+      steps {
+        echo 'y' | ${toolbelt}sfdx plugins:install sfpowerkit
+      }
+    }
